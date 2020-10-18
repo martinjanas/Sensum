1120626df435a1ef4b3f09191cbee97c1fec8698
MartiNJ409 <44605473+martinjanas@users.noreply.github.com>
2020-10-18 12:23:45 +0200

    Add files via upload

--- src/valve_sdk/interfaces/CClientState.h
+++ src/valve_sdk/interfaces/CClientState.h
@@ -29,7 +29,13 @@ public:
 	uint32_t m_nClientTick;
 };
 	
-	
+
+class CClientState {
+public:
+	/*void ForceFullUpdate() {
+		*reinterpret_cast<int*>(std::uintptr_t(this) + 0x174) = -1;
+	}*/
+
 	char pad000[0x9C];
 	INetChannel* m_NetChannel;
 	int	m_nChallengeNr;
@@ -43,7 +49,7 @@ public:
 	int m_nServerCount;
 	int m_nCurrentSequence;
 	char pad004[0x8];
-	CClockDriftManager m_ClockDriftMgr;
+	CClockDriftMgr m_ClockDriftMgr;
 	int m_nDeltaTick;
 	char pad005[0x4];
 	int m_nViewEntity;
@@ -69,60 +75,12 @@ public:
 	char pad009[0x4B];
 	QAngle viewangles;
 	char pad010[0xCC];
-	CEventInfo* m_events;
-	
+	void* m_events;
+
 	void ForceFullUpdate() {
 		m_nDeltaTick = -1;
 	}
 };
-
-/*class CClientState {
-public:
-	/*void ForceFullUpdate() {
-		*reinterpret_cast<int*>(std::uintptr_t(this) + 0x174) = -1;
-	}*/
-
-	char pad_0000[156];             //0x0000
-	INetChannel* m_NetChannel;      //0x009C
-	uint32_t m_nChallengeNr;        //0x00A0
-	char pad_00A4[100];             //0x00A4
-	uint32_t m_nSignonState;        //0x0108
-	char pad_010C[8];               //0x010C
-	float m_flNextCmdTime;          //0x0114
-	uint32_t m_nServerCount;        //0x0118
-	uint32_t m_nCurrentSequence;    //0x011C
-	char pad_0120[8];               //0x0120
-	CClockDriftMgr m_ClockDriftMgr; //0x0128
-	uint32_t m_nDeltaTick;          //0x0174
-	bool m_bPaused;                 //0x0178
-	char pad_017D[3];               //0x017D
-	uint32_t m_nViewEntity;         //0x0180
-	uint32_t m_nPlayerSlot;         //0x0184
-	char m_szLevelName[260];        //0x0188
-	char m_szLevelNameShort[80];    //0x028C
-	char m_szGroupName[80];         //0x02DC
-	char pad_032Ñ[92];              //0x032Ñ
-	uint32_t m_nMaxClients;         //0x0388
-	char pad_0314[18824];           //0x0314
-	float m_flLastServerTickTime;   //0x4C98
-	bool insimulation;              //0x4C9C
-	char pad_4C9D[3];               //0x4C9D
-	uint32_t oldtickcount;          //0x4CA0
-	float m_tickRemainder;          //0x4CA4
-	float m_frameTime;              //0x4CA8
-	int lastoutgoingcommand;        //0x4CAC
-	int chokedcommands;             //0x4CB0
-	int last_command_ack;           //0x4CB4
-	int command_ack;                //0x4CB8
-	int m_nSoundSequence;           //0x4CBC
-	char pad_4CC0[80];              //0x4CC0
-	QAngle viewangles;              //0x4D10
-	char pad_4D1C[208];             //0x4D1C
-
-	void ForceFullUpdate() {
-		m_nDeltaTick = -1;
-	}
-}; */
 
 #pragma pack(pop)
 
