89f41626e459b53a047abb210b3dbaa781c8e91e
onixiya1337 <unconfigured@null.spigotmc.org>
2021-05-09 15:28:20 +0200

    Fixed loading settings for Molotov Helper and HE Grenade Helper

--- src/settings/settings.cpp
+++ src/settings/settings.cpp
@@ -408,7 +408,7 @@ namespace settings
 				Option::Load(root["misc.smoke_helper"], misc::smoke_helper);
 				Option::Load(root["misc.flash_helper"], misc::flash_helper);
 				Option::Load(root["misc.hegrenade_helper"], misc::hegrenade_helper);
-				Option::Load(root["misc.molotov_helper"], misc::hegrenade_helper);
+				Option::Load(root["misc.molotov_helper"], misc::molotov_helper);
 				Option::Load(root["misc.selfnade"], misc::selfnade);
 
 				Option::Load(root["glow.enemy"], glow::enemy::enabled);
@@ -659,7 +659,7 @@ namespace settings
 				config["misc.smoke_helper"] = misc::smoke_helper;
 				config["misc.flash_helper"] = misc::flash_helper;
 				config["misc.hegrenade_helper"] = misc::hegrenade_helper;
-				config["misc.molotov_helper"] = misc::hegrenade_helper;
+				config["misc.molotov_helper"] = misc::molotov_helper;
 				config["misc.selfnade"] = misc::selfnade;
 
 				Option::Save(config["misc_spread_crosshair_color"], settings::visuals::spread_cross_color);
