8703aae0b959535399e86b1bbb0831a00f7ca152
martinjanas <martin409@seznam.cz>
2024-11-21 10:53:27 +0100

    Update CreateMove.cpp

--- src/hooks/functions/CreateMove.cpp
+++ src/hooks/functions/CreateMove.cpp
@@ -2,10 +2,9 @@
 #include "../../sdk/sdk.h"
 #include "../../features/features.h"
 
-bool __fastcall hooks::createmove_csgoinput::hooked(void* rcx, int slot, CUserCmd* cmd, float curtime) //CUserCmd is null, todo: take a look in ida
+bool __fastcall hooks::createmove_csgoinput::hooked(void* rcx, int slot, CUserCmd* cmd, float curtime)
 {
     features::aimbot::handle(cmd);
 
     return original_fn(rcx, slot, cmd, curtime);
 }
-
